f07a6bd4a2bdffe94b95b99c971772b29f2f9264
Prashant kashyap <Prashantkashyap12@gmail.com>
2023-03-24 20:53:53 +0530

    Shiva_TEST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ShivaSax';
+
+  firstName:any;
+  FContact:any;
+  FselectBank:any;
+  mainUPI:any;
+  tempID:any;
+  tempAlert:boolean = false;
+  time:any;
+
+  private tempURL = 'https://ajay-bbdd1-default-rtdb.firebaseio.com/Shiva.json';
+
+  constructor(private _http:HttpClient){}
+
+  ngOnInit(){
+    var mainRandom = Math.floor(Math.random() * (2500 - 5500) ) + 118987987465 ;
+    this.tempID = mainRandom;
+  }
+
+
+  temp() {
+    this._http.post(this.tempURL,{
+      Name: this.firstName,
+      Contact: this.FContact,
+      Bank: this.FselectBank,
+      UPI: this.mainUPI,
+    })
+    .subscribe(res=>{
+      console.log(res);
+      this.tempAlert = true;
+    })
+  }
+
+
+
 }
